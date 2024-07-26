f7cf17d228aa68d838ac7601b7d37674593e9b70
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-07-26 01:08:15 -0700

    fourth commit for question 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,24 +5,25 @@ import sys
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total.
+    Args:
+    coins (list of int): A list of the values of the coins in your possession.
+    total (int): The total amount to be made with the coins.
+
+    Returns:
+    int: The fewest number of coins needed to meet the total amount.
     """
     if total <= 0:
         return 0
 
-    # list to store the minimum number of coins for each amount of total
-    dp = [sys.maxsize for _ in range(total + 1)]
+    # Initialize the dp array
+    dp = [sys.maxsize] * (total + 1)
     dp[0] = 0  # Base case: No coins needed to make a total of 0
 
-    # Number of different coin denominations
-    m = len(coins)
-
-    # Fill the dp table in a bottom-up manner
     for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = dp[i - coins[j]]
+        for coin in coins:
+            if coin <= i:
+                subres = dp[i - coin]
                 if subres != sys.maxsize and subres + 1 < dp[i]:
                     dp[i] = subres + 1
 
-    # If dp[total] still sys.maxsize, then total can't be met with given coins
     return -1 if dp[total] == sys.maxsize else dp[total]
